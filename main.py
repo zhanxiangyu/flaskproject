d512b50f37ca46aeeef0d97ad278f0948d398590
zhanxiangyu <1033432955@qq.com>
2018-05-23 15:42:56 +0800

    study chapter

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
 from config import DevConfig
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+bootstrap = Bootstrap(app)
+monent = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
-def home():
-    return '<h1>hello world</h1>'
+def index():
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
