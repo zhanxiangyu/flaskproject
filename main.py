fe8e923dd4b5ecc219f3a5c13a665a3903116e4f
zhanxiangyu <1033432955@qq.com>
2018-05-23 17:47:42 +0800

    update

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 # -*- coding:utf-8 -*-
+import os
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from config import DevConfig
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'asdfasdfasdfxxxxxxxxxxxxfas'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_object(DevConfig)
 bootstrap = Bootstrap(app)
 monent = Moment(app)
+db = SQLAlchemy(app)
+
+# db.drop_all() 删除表
+# db.create_all() 创建表
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return "<role %r>" % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is you name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -21,9 +60,22 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False),
                            current_time=datetime.utcnow())
 
 
