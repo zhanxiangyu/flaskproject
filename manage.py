426073569f750b97b1939d596cc16b1564b4e19c
zhanxiangyu <1033432955@qq.com>
2018-05-24 09:10:10 +0800

    update chapter5

--- manage.py
+++ manage.py
@@ -1,15 +1,18 @@
 # -*- coding:utf-8 -*-
 from flask_script import Manager, Server
-from main import app
+from flask_migrate import Migrate, MigrateCommand
+from main import app, db, User, Role
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 manager.add_command('runserver', Server())
+manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db, User=User, Role=Role)
 
 
 if __name__ == '__main__':
