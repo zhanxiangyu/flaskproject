b53fceca3c41e58170415e49678d403ee9d6f290
zhanxiangyu <1033432955@qq.com>
2018-05-26 15:59:29 +0800

    update7

--- config.py
+++ config.py
@@ -1,13 +1,47 @@
 # -*- coding:utf-8 -*-
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'asdfasdfasdfxxxxxxxxxxxxfas'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <15797738965@sina.cn>'
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
-class ProdConfig(Config):
-    pass
-
-
-class DevConfig(Config):
+class DevelopmentConfig(Config):
     DEBUG = True
+    MAIL_SERVER = 'smtp.sina.cn'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SENDER = os.environ.get("MAIL_SENDER")
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
--- manage.py
+++ manage.py
@@ -1,18 +1,60 @@
 # -*- coding:utf-8 -*-
-from flask_script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand
-from main import app, db, User, Role
+# from flask_script import Manager, Server
+# from flask_migrate import Migrate, MigrateCommand
+# from main import app, db, User, Role
+#
+# manager = Manager(app)
+# migrate = Migrate(app, db)
+#
+# manager.add_command('runserver', Server())
+# manager.add_command('db', MigrateCommand)
+#
+#
+# @manager.shell
+# def make_shell_context():
+#     return dict(app=app, db=db, User=User, Role=Role)
+#
+#
 
+import os
+from app import create_app, db
+from app.models import User, Role
+from flask.ext.script import Manager, Shell, Server
+from flask.ext.migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
-manager.add_command('runserver', Server())
-manager.add_command('db', MigrateCommand)
 
-
-@manager.shell
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command('runserver', Server())
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+# import os
+# from flask_migrate import Migrate
+# from app import create_app, db
+# from app.models import User, Role
+#
+# app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+# migrate = Migrate(app, db)
+#
+#
+# @app.shell_context_processor
+# def make_shell_context():
+#     return dict(db=db, User=User, Role=Role)
+#
+#
+# @app.cli.command()
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
