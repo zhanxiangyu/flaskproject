d6628a2832fb21de2ba15b8dc50f22082c64e183
zhanxiangyu <1033432955@qq.com>
2018-06-05 16:11:03 +0800

    update chapter8 to 8e

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,82 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
+from .forms import LoginForm, RegisterForm
+from ..models import User
+from .. import db
+from ..email import send_email
 
 
-@auth.route('/login')
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirm you account! Thanks')
+    else:
+        flash('The confirm link is invalid, please send again!')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirm_token()
+    send_email(current_user.email, 'Resend Confirm You Account', 'auth/email/confirm', user=current_user, token=token)
+    flash('han send confirm email to you again')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('invalid username and password')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logout')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    register_form = RegisterForm()
+    if register_form.validate_on_submit():
+        user = User(username=register_form.username.data, email=register_form.email.data,
+                    password=register_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirm_token()
+        send_email(user.email, 'Confirm You Account', 'auth/email/confirm', user=user, token=token)
+        flash("a confirmation email has been send to you by email..")
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', form=register_form)
+
